84905d89207e5980224aa091cb5b1d9c71436464
Abdul Rahman <heartattackegypt19@gmail.com>
2024-10-02 05:20:55 +0300

    update

--- bedoReport.py
+++ bedoReport.py
@@ -3,15 +3,27 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from pymongo import MongoClient
 from datetime import datetime, timedelta
+import os
 
+
+MONGO_URI = os.getenv('MONGO_URI')
+GMAIL_USER = os.getenv('GMAIL_USER')
+GMAIL_PASSWORD = os.getenv('GMAIL_PASSWORD')
+
+current_time = datetime.now().time()
+start_time = datetime.now().replace(hour=11, minute=0, second=0, microsecond=0).time()
+end_time = datetime.now().replace(hour=3, minute=0, second=0, microsecond=0).time()
+
+if not (start_time <= current_time or current_time <= end_time):
+    print("Script is running outside the allowed time range (11:00 AM to 3:00 AM). Exiting.")
+    exit()
 # MongoDB connection settings
-MONGO_URI = "mongodb+srv://api-application:9RMeQ08hTGRFtL2i@cluster0.jcitrgd.mongodb.net/alaadev?retryWrites=true&w=majority"  # Add your MongoDB URI here
 client = MongoClient(MONGO_URI)
 db = client['alaadev']
 
 # Gmail credentials
-GMAIL_USER = 'bedojobs@gmail.com'
-GMAIL_PASSWORD = 'bvkf indh uthn toel'
+
+
 
 
 # Function to send an email
